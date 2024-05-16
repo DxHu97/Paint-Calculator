4634da5328d3b0a443dd1c48d7593c27478291a1
David Hu <davidhu52497@gmail.com>
2024-05-16 13:17:17 -0400

    Made module with JSON and added CalculatePrice functionality

--- CalculatePrice.js
+++ CalculatePrice.js
@@ -1,10 +1,21 @@
 class CalculatePrice{
-    constructor(price, buckets, area){
+    constructor(price, buckets, coverage, area = null){
         this.price = price;
-        this.buckets = buckets;
+        this.buckets = 0;
+        this.coverage = coverage;
         this.area = null;
     }
+
+    setArea(area){
+        this.area = area;
+    }
+
+    amountOfBucketsNeeded(){
+        this.buckets = Math.ceil(this.area/this.coverage);
+    }
     calculateTotalCost(){
-        
+        return this.price * this.buckets;
     }
 }
+
+export default CalculatePrice;
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //ask for how many walls, call the user input that many times
-import CalculateArea from "./CalculateArea";
+import CalculateArea from "./CalculateArea.js";
+import CalculatePrice from "./CalculatePrice.js";
 
 const areaTotal = new CalculateArea();
 areaTotal.addWall(5,10)
@@ -7,10 +8,20 @@ areaTotal.addWall(15,1)
 console.log(areaTotal.walls)
 const area = areaTotal.calculateTotalWallArea();
 console.log(area)
-areaTotal.addObstacles(1,20);
-areaTotal.addObstacles(5,20);
+areaTotal.addObstacles(1,1);
+areaTotal.addObstacles(1,1);
 const oArea = areaTotal.calculateTotalObstacleArea();
 console.log(oArea);
 const totalArea = areaTotal.calculateTotalAreaPaintable();
 console.log(totalArea);
+
+const priceTotal = new CalculatePrice(50,0,10);
+priceTotal.setArea(totalArea);
+console.log("Area:", priceTotal.area)
+priceTotal.amountOfBucketsNeeded();
+const totalPrice = priceTotal.calculateTotalCost();
+console.log(totalPrice)
+
+
+
 
