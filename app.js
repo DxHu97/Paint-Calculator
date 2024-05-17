1671bae790f44c376b579fe1323cecf6d214ae9e
David Hu <davidhu52497@gmail.com>
2024-05-17 07:31:36 -0400

    Working basic paint calculator, added in prompt

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 //ask for how many walls, call the user input that many times
 import CalculateArea from "./CalculateArea.js";
 import CalculatePrice from "./CalculatePrice.js";
+import promptSync from 'prompt-sync';
+import promptSyncHistory from 'prompt-sync-history';
+
+const prompt = promptSync({ history: promptSyncHistory() });
+
+function askQuestion(query) {
+    return prompt(query);
+}
 
 const areaTotal = new CalculateArea();
-areaTotal.addWall(5,10)
+/*areaTotal.addWall(5,10)
 areaTotal.addWall(15,1)
-console.log(areaTotal.walls)
+console.log(areaTotal.walls)*/
+const wallsCount = askQuestion("How many walls do you want to paint?")
+for (let i = 0; i < wallsCount; i++){
+    areaTotal.addWall(askQuestion("From left to right how long in feet"), askQuestion("From top to bottom how tall in feet"))
+    console.log("Area array:", areaTotal.walls)
+}
+const obstacleCount = askQuestion("How many obstacles are there in the walls?")
+for (let i = 0; i < obstacleCount; i++){
+    areaTotal.addObstacles(askQuestion("From left to right how long are the obstacles in feet"), askQuestion("From top to bottom how tall are the obstacles in feet"))
+    console.log("Obstacles array:", areaTotal.obstacles)
+}
 const area = areaTotal.calculateTotalWallArea();
-console.log(area)
+console.log("Wall area:", area)
+/*areaTotal.addObstacles(1,1);
 areaTotal.addObstacles(1,1);
-areaTotal.addObstacles(1,1);
+*/
 const oArea = areaTotal.calculateTotalObstacleArea();
-console.log(oArea);
+console.log("Obstacle area:", oArea);
 const totalArea = areaTotal.calculateTotalAreaPaintable();
-console.log(totalArea);
+console.log("Total area paintable:", totalArea);
 
 const priceTotal = new CalculatePrice(50,0,10);
 priceTotal.setArea(totalArea);
-console.log("Area:", priceTotal.area)
 priceTotal.amountOfBucketsNeeded();
 const totalPrice = priceTotal.calculateTotalCost();
-console.log(totalPrice)
+console.log("Total price:", totalPrice)
 
 
 
