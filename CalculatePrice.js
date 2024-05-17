c22a7c2127967c5f4cfc907ea7c146aac465e70b
David Hu <davidhu52497@gmail.com>
2024-05-17 08:21:46 -0400

    Added price functionality with real paint prices

--- CalculatePrice.js
+++ CalculatePrice.js
@@ -1,8 +1,8 @@
 class CalculatePrice{
-    constructor(price, buckets, coverage, area = null){
+    constructor(price, buckets, area = null){
         this.price = price;
         this.buckets = 0;
-        this.coverage = coverage;
+        //this.coverage = coverage;
         this.area = null;
     }
 
@@ -11,8 +11,19 @@ class CalculatePrice{
     }
 
     amountOfBucketsNeeded(){
-        this.buckets = Math.ceil(this.area/this.coverage);
+        const litersNeeded = Math.ceil(this.area/10);
+        if (litersNeeded === 0){
+            this.buckets = 0;
+            this.price = 0;
+        } else if (litersNeeded < 5){
+            this.buckets = 1;
+            this.price = 11;
+        } else {
+            const bucketsNeeded = litersNeeded/10
+            this.buckets = bucketsNeeded;
+            this.price = 15
     }
+}
     calculateTotalCost(){
         return this.price * this.buckets;
     }
--- app.js
+++ app.js
@@ -1,45 +1,52 @@
 //ask for how many walls, call the user input that many times
 import CalculateArea from "./CalculateArea.js";
 import CalculatePrice from "./CalculatePrice.js";
-import promptSync from 'prompt-sync';
-import promptSyncHistory from 'prompt-sync-history';
+import promptSync from "prompt-sync";
+import promptSyncHistory from "prompt-sync-history";
 
 const prompt = promptSync({ history: promptSyncHistory() });
 
 function askQuestion(query) {
-    return prompt(query);
+  console.log(query);
+  return prompt("> ");
 }
 
-const areaTotal = new CalculateArea();
-/*areaTotal.addWall(5,10)
-areaTotal.addWall(15,1)
-console.log(areaTotal.walls)*/
-const wallsCount = askQuestion("How many walls do you want to paint?")
-for (let i = 0; i < wallsCount; i++){
-    areaTotal.addWall(askQuestion("From left to right how long in feet"), askQuestion("From top to bottom how tall in feet"))
-    console.log("Area array:", areaTotal.walls)
+async function main() {
+  try {
+    const areaTotal = new CalculateArea();
+    const wallsCount = askQuestion("How many walls do you want to paint?");
+    for (let i = 0; i < wallsCount; i++) {
+      areaTotal.addWall(
+        askQuestion("From left to right how long in meters"),
+        askQuestion("From top to bottom how tall in meters")
+      );
+      console.log("Area array:", areaTotal.walls);
+    }
+    const obstacleCount = askQuestion(
+      "How many obstacles are there in the walls?"
+    );
+    for (let i = 0; i < obstacleCount; i++) {
+      areaTotal.addObstacles(
+        askQuestion("From left to right how long are the obstacles in meters"),
+        askQuestion("From top to bottom how tall are the obstacles in meters")
+      );
+      console.log("Obstacles array:", areaTotal.obstacles);
+    }
+    const area = areaTotal.calculateTotalWallArea();
+    console.log("Wall area:", area);
+    const oArea = areaTotal.calculateTotalObstacleArea();
+    console.log("Obstacle area:", oArea);
+    const totalArea = areaTotal.calculateTotalAreaPaintable();
+    console.log("Total area paintable:", totalArea);
+
+    const priceTotal = new CalculatePrice(50, 0);
+    priceTotal.setArea(totalArea);
+    priceTotal.amountOfBucketsNeeded();
+    const totalPrice = priceTotal.calculateTotalCost();
+    console.log("Total price in pounds:", totalPrice);
+  } catch (error) {
+    console.error('Something went wrong');
+  }
 }
-const obstacleCount = askQuestion("How many obstacles are there in the walls?")
-for (let i = 0; i < obstacleCount; i++){
-    areaTotal.addObstacles(askQuestion("From left to right how long are the obstacles in feet"), askQuestion("From top to bottom how tall are the obstacles in feet"))
-    console.log("Obstacles array:", areaTotal.obstacles)
-}
-const area = areaTotal.calculateTotalWallArea();
-console.log("Wall area:", area)
-/*areaTotal.addObstacles(1,1);
-areaTotal.addObstacles(1,1);
-*/
-const oArea = areaTotal.calculateTotalObstacleArea();
-console.log("Obstacle area:", oArea);
-const totalArea = areaTotal.calculateTotalAreaPaintable();
-console.log("Total area paintable:", totalArea);
 
-const priceTotal = new CalculatePrice(50,0,10);
-priceTotal.setArea(totalArea);
-priceTotal.amountOfBucketsNeeded();
-const totalPrice = priceTotal.calculateTotalCost();
-console.log("Total price:", totalPrice)
-
-
-
-
+main();
